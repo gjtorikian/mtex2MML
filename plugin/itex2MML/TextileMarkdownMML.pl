e69c21878437fb0e943c10ce800fd0b25f0928ca
Jacques Distler <distler@golem.ph.utexas.edu>
2007-01-17 01:00:16 -0600

    Update MovableType plugin to support equation numbering and cross-referencing. Bundle the TextileMarkdownMML plugin.

---
+++ plugin/itex2MML/TextileMarkdownMML.pl
@@ -0,0 +1,83 @@
+# TextileMarkdownMML.pl
+#
+# Provides two MT text filters
+#
+#   Textile+itex2MML
+#  Markdown+itex2MML
+#
+# You must have the itex2MML, Textile 1.1 and Markdown filters already installed
+# This filter first runs your text through itex2MML and then through
+# either Textile or Markdown.
+#
+# The Textile+itex2MML filter requires a small patch to the Textile 1.1 plugin.
+# See:
+#     http://golem.ph.utexas.edu/~distler/blog/archives/000374.html
+#
+# Based on textileMML.pl, by Yuan-Chung Cheng.
+#
+# Copyright, 2004-2007, Jacques Distler.
+# This code is released under the GPL
+#
+
+package MT::Plugins::TextileMarkdownMML;
+
+use vars qw($VERSION);
+$VERSION = 1.2;
+
+use strict;
+
+use MT;
+use MT::Template::Context;
+use MT::Plugin::itex2MML;
+
+eval{ require MT::Plugin;};
+unless ($@) {
+   my $plugin = {
+      name => "TextileMarkdownMML",
+      version => $VERSION,
+      description => "A Text-Filter, adding support for embedded itex equations to Markdown and/or Textile 1.1. Requires itex2MML and one or both of the latter plugins be installed.",
+      doc_link => 'http://golem.ph.utexas.edu/~distler/blog/archives/000374.html',
+   };
+   MT->add_plugin(new MT::Plugin($plugin));
+}
+
+my $filters = MT->all_text_filters();
+
+if (exists($filters->{'textile_1'})) {
+	MT->add_text_filter('textile_1MML' => {
+				     label => 'Textile with itex to MathML',
+				     on_format => sub { &textileMML; },
+				    });
+}
+if (exists($filters->{'markdown'})) {
+	MT->add_text_filter('markdownMML' => {
+				     label => 'Markdown with itex to MathML',
+				     on_format => sub { &markdownMML; },
+				    });
+}
+
+sub textileMML {
+
+  my $text=shift;
+  my $ctx=shift;
+
+  # now call apply_text_filters() to do the job
+  $text=MT->apply_text_filters($text, ['itexToMML'], $ctx);
+  $text=MT->apply_text_filters($text, ['textile_1'], $ctx);
+
+  $text;
+}
+
+sub markdownMML {
+
+  my $text=shift;
+  my $ctx=shift;
+
+  # now call apply_text_filters() to do the job
+  $text=MT->apply_text_filters($text, ['itexToMML'], $ctx);
+  $text=MT->apply_text_filters($text, ['markdown'], $ctx);
+
+  $text;
+}
+1;
+
---
+++ plugin/itex2MML/itex2MML.pl
@@ -0,0 +1,125 @@
+# itex2MML
+# version 1.0
+# copyright 2003-2007, Jacques Distler
+#
+
+package MT::Plugin::itex2MML;
+
+use MT;
+use File::Temp qw(tempfile);
+
+use vars qw( $VERSION );
+$VERSION = '1.0';
+
+eval{ require MT::Plugin;};
+unless ($@) {
+   my $plugin = {
+      name => "itex2MML",
+      version => $VERSION,
+      description => "A Text-Filter, translating embedded itex equations into MathML",
+      doc_link => 'http://golem.ph.utexas.edu/~distler/blog/itex2MML.html',
+   };
+   MT->add_plugin(new MT::Plugin($plugin));
+}
+
+MT->add_text_filter(itexToMML => {
+	label => 'itex to MathML',
+	on_format => sub { &itexToMML; },
+});
+MT->add_text_filter(itexToMMLpara => {
+        label => 'itex to MathML with parbreaks',
+        on_format => sub { &itexToMMLpara; },
+});
+
+my $itex2mml_number_equations = 1;
+my $itex2mml_binary = "/usr/local/bin/itex2MML";
+
+sub itexToMML {
+    $_=shift;
+    $ctx = shift;
+    $_=~ s/\r//g;
+    $_ = number_equations($_, $itex2mml_number_equations, $ctx);
+    my ($Reader,$outfile) = tempfile( UNLINK => 1 );
+    my ($Writer,$infile) = tempfile( UNLINK => 1 );
+    print $Writer "$_";
+    system("$itex2mml_binary <$infile >$outfile");
+    my @out = <$Reader>;
+    close $Reader;
+    close $Writer;
+    eval {unlink ($infile, $outfile);};
+    join('',@out);
+}
+
+sub itexToMMLpara {
+    $_ = shift;
+    $ctx = shift;
+    $_=~ s/\r//g;
+    $_ = number_equations($_, $itex2mml_number_equations, $ctx);
+    $_ = splitparas($_);
+    my ($Reader,$outfile) = tempfile( UNLINK => 1 );
+    my ($Writer,$infile) = tempfile( UNLINK => 1 );
+    print $Writer "$_";
+    system("$itex2mml_binary <$infile >$outfile");
+    my @out = <$Reader>;
+    close $Reader;
+    close $Writer;
+    eval {unlink ($infile, $outfile);};
+    join('',@out);
+}
+
+sub splitparas {
+    my $str = shift;
+    $str ||= '';
+    my @paras = split /\n{2,}/, $str;
+    for my $p (@paras) {
+        if ($p !~ m@^</?(?:h1|h2|h3|h4|h5|h6|table|ol|dl|ul|menu|dir|p|pre|center|form|fieldset|select|blockquote|address|div|hr)@) {
+            $p = "<p>$p</p>";
+        }
+    }
+    join "\n\n", @paras;
+}
+
+sub number_equations {
+  $_ = shift;
+  my $arg_value = shift;
+  my $ctx = shift;
+
+  if ($arg_value == 0) {return $_;}
+  
+  my $prefix = "eq";
+  if ((defined $ctx)  && (ref($ctx) eq 'MT::Template::Context')) {
+    if ($ctx->stash('comment') ) {
+       $prefix = "c" . $ctx->stash('comment')->id;
+    } elsif ($ctx->stash('entry') ) {
+       $prefix = "e" . $ctx->stash('entry')->id;
+    }
+  }
+  my $cls = "numberedEq";
+
+  my %eqnumber;
+  my $eqno=1;
+
+  # add equation numbers to \[...\]
+  #  - introduce a wrapper-<div> and a <span> with the equation number
+  while (s/\\\[(.*?)\\\]/\n<div class=\"$cls\"><span>\($eqno\)<\/span>\$\$$1\$\$<\/div>\n/s) {
+    $eqno++;
+  }
+
+  # assemble equation labels into a hash
+  # - remove the \label{} command, collapse surrounding whitespace
+  # - add an ID to the wrapper-<div>. prefix it to give a fighting chance
+  #   for the ID to be unique
+  # - hash key is the equation label, value is the equation number
+  while (s/<div class=\"$cls\"><span>\((\d+)\)<\/span>\$\$((?:[^\$]|\\\$)*)\s*\\label{(\w*)}\s*((?:[^\$]|\\\$)*)\$\$<\/div>/<div class=\"$cls\" id=\"$prefix:$3\"><span>\($1\)<\/span>\$\$$2$4\$\$<\/div>/s) {
+    $eqnumber{"$3"} = $1;
+  }
+
+  # add cross-references
+  # - they can be either (eq:foo) or \eqref{foo}
+  s/\(eq:(\w+)\)/\(<a href=\"#$prefix:$1\">$eqnumber{"$1"}<\/a>\)/g;
+  s/\\eqref\{(\w+)\}/\(<a href=\'#$prefix:$1\'>$eqnumber{"$1"}<\/a>\)/g;
+  
+  return $_;
+}
+
+1;
