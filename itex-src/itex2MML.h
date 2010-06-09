928179acaed5592b139eb9ad83b1b85728b1525c
Jacques Distler <distler@golem.ph.utexas.edu>
2010-06-09 13:23:36 -0500

    itex2MML 1.3.25
    Support \boxed{}.
    Rationalize support for \slashed{}
    In Rubygem, add itex_stringsupport.rb.

--- itex-src/itex2MML.h
+++ itex-src/itex2MML.h
@@ -1,11 +1,11 @@
-/*             itex2MML 1.3.24
- *   itex2MML.h last modified 5/24/2010
+/*             itex2MML 1.3.25
+ *   itex2MML.h last modified 6/9/2010
  */
 
 #ifndef ITEX2MML_H
 #define ITEX2MML_H
 
-#define ITEX2MML_VERSION "1.3.24"
+#define ITEX2MML_VERSION "1.3.25"
 
 #ifdef __cplusplus
 extern "C" {
