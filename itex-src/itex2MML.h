7bdcc3fbb0df176fcb6fa2d0042cfa9b37be269d
Jacques Distler <distler@golem.ph.utexas.edu>
2010-09-07 11:54:25 -0500

    itex2MML 1.4.0: Extensible Arrows
    We now support extensible arrows,
    with LaTeX-style optional arguments.
    
    \xrightarrow[a]{b}
    
    is (in MathML, though not in LaTeX)
    equivalent to
    
    \underoverset{a}{b}{\rightarrow}
    
    while
    
    \xrightarrow{b}
    
    is (in MathML, though not in LaTeX)
    equivalent to
    
    \overset{b}{\rightarrow}
    
    The supported arrows are
    
    \xrightarrow
    \xleftarrow
    \xleftrightarrow
    \xLeftarrow
    \xRightarrow
    \xLeftrightarrow
    \xleftrightharpoons
    \xrightleftharpoons
    \xhookleftarrow
    \xhookrightarrow
    \xmapsto

--- itex-src/itex2MML.h
+++ itex-src/itex2MML.h
@@ -1,11 +1,11 @@
-/*             itex2MML 1.3.27
- *   itex2MML.h last modified 9/3/2010
+/*             itex2MML 1.4.0
+ *   itex2MML.h last modified 9/7/2010
  */
 
 #ifndef ITEX2MML_H
 #define ITEX2MML_H
 
-#define ITEX2MML_VERSION "1.3.27"
+#define ITEX2MML_VERSION "1.4.0"
 
 #ifdef __cplusplus
 extern "C" {
