487bef6c4fe1b598b20827cbea75d8c9fdead183
Jacques Distler <distler@golem.ph.utexas.edu>
2010-09-30 14:34:46 -0500

    Nested Extensible Arrows
    Now you can nest extensible arrows, with
    optional arguments, in a LaTeX-compatible
    fashion.

--- itex-src/itex2MML.h
+++ itex-src/itex2MML.h
@@ -1,11 +1,11 @@
-/*             itex2MML 1.4.3
- *   itex2MML.h last modified 9/29/2010
+/*             itex2MML 1.4.4
+ *   itex2MML.h last modified 9/30/2010
  */
 
 #ifndef ITEX2MML_H
 #define ITEX2MML_H
 
-#define ITEX2MML_VERSION "1.4.2"
+#define ITEX2MML_VERSION "1.4.4"
 
 #ifdef __cplusplus
 extern "C" {
