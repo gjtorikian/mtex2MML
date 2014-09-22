3f0a062e88ae06de3ae45101600d94b991d92c83
Garen Torikian <gjtorikian@gmail.com>
2014-09-21 19:04:42 -0700

    Add fixture file structure to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
 $:.unshift File.expand_path( File.join( File.dirname( __FILE__ ), "../lib" ) )
 require 'itextomml'
 require 'minitest/autorun'
+
+def fixture_file(name)
+  File.read( File.join(File.dirname( __FILE__ ), "fixtures", "#{name}.html") ).strip
+end
