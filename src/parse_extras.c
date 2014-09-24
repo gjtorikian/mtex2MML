a0e698cf71d11f1a30ff4b6725e56aae3b8b3ad7
Garen Torikian <gjtorikian@gmail.com>
2014-09-24 15:03:21 -0700

    Iterate on array environments with a basic stack and pop
    This improves the hline detection by allowing nested arrays to come
    through.

--- src/parse_extras.c
+++ src/parse_extras.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "parse_extras.h"
+#include "stack.h"
 
 char *replace_str(const char *str, const char *old, const char *new) {
   char *ret, *r;
@@ -51,7 +54,7 @@ char *substring(char *inpStr, int startPos, int strLen) {
 
   if (strLen < 0)
       strLen = 0;
-  if (startPos >strlen (inpStr))
+  if (startPos > strlen (inpStr))
       startPos = strlen (inpStr);
 
   /* Adjust length if source string too short. */
@@ -92,60 +95,158 @@ void remove_last_char(char* str) {
   str[len-1] = 0;
 }
 
-void insert_substring(char *str, char *new_str, int position) {
-   char *f, *e;
-   size_t length = strlen(str);
+void insert_substring(char **dest, char *ins, size_t location) {
+  size_t origsize = 0;
+  size_t resize = 0;
+  size_t inssize = 0;
+  size_t destsize = strlen(&dest) + 1;
 
-   f = substring(str, 1, position - 1 );
-   e = substring(str, position, length-position+1);
+  if (!dest || !ins)
+      return;  // invalid parameter
 
-   strcpy(str, "");
-   strcat(str, f);
-   free(f);
-   strcat(str, new_str);
-   strcat(str, e);
-   free(e);
+  if (strlen(ins) == 0)
+    return; // invalid parameter
+
+  origsize = strlen(*dest);
+  inssize = strlen(ins);
+  resize = strlen(*dest) + inssize + 1; // 1 for the null terminator
+
+  if (location > origsize)
+    return; // invalid location, out of original string
+
+  // resize string to accommodate inserted string if necessary
+  if (destsize < resize)
+    *dest = (char*)realloc(*dest, resize);
+
+  // move string to make room for insertion
+  memmove(&(*dest)[location+inssize], &(*dest)[location], origsize - location);
+  (*dest)[origsize + inssize] = '\0'; // null terminate string
+
+  // insert string
+  memcpy(&(*dest)[location], ins, inssize);
+}
+
+void strrev(char *str) {
+  char temp, *end_ptr;
+
+  /* If str is NULL or empty, do nothing */
+  if( str == NULL || !(*str) )
+    return;
+
+  end_ptr = str + strlen(str) - 1;
+
+  /* Swap the chars */
+  while( end_ptr > str ) {
+    temp = *str;
+    *str = *end_ptr;
+    *end_ptr = temp;
+    str++;
+    end_ptr--;
+  }
+}
+
+void initHlineDataArray(hlineDataArray *a, size_t initialSize)
+{
+  // Allocate initial space
+  a->array = (hlineData *)malloc(initialSize * sizeof(hlineData));
+
+  a->used = 0;           // no elements used
+  a->size = initialSize; // available number of elements
+
+  // Initialize all elements of the array at once: they are contiguous
+  memset(&a->array[0], 0, sizeof(hlineData) * initialSize);
+}
+
+void insertHlineDataArray(hlineDataArray *a, hlineData element)
+{
+  if (a->used == a->size) {
+      a->size *= 2;
+      a->array = (hlineData *)realloc(a->array, a->size * sizeof(hlineData));
+      // Initialize the last/new elements of the reallocated array
+      memset(&a->array[a->used],0, sizeof(hlineData) * (a->size - a->used));
+  }
+
+  a->array[a->used].attr_strings = (char*)malloc(strlen(element.attr_strings) + 1);
+  strcpy(a->array[a->used].attr_strings, element.attr_strings);
+
+  a->array[a->used].offset_pos = element.offset_pos;
+
+  a->used++;
+}
+
+void sortHLineDataArray(hlineDataArray *a) {
+  int i, j, n = a->used;
+
+  for(i = 1;i < n; i++) {
+    for(j = 0;j < n - i; j++) {
+      if(a->array[j].offset_pos < a->array[j+1].offset_pos) {
+          hlineData temp = a->array[j];
+          a->array[j] = a->array[j+1];
+          a->array[j+1] = temp;
+      }
+    }
+  }
+}
+
+void deleteHlineDataArray(hlineDataArray *a)
+{
+  // Free all name variables of each array element first
+  for(int i = 0; i <a->used; i++)
+  {
+    free(a->array[i].attr_strings);
+    a->array[i].attr_strings = NULL;
+  }
+
+  // Now free the array
+  free(a->array);
+  a->array = NULL;
+
+  a->used = 0;
+  a->size = 0;
 }
 
 char * hline_replace(const char *string) {
+  stackT array_stack;
   char *tok = NULL, *chunk = NULL;
   char *newstr = strdup(string);
+  char *line = strtok(strdup(string), "\n");
   const char *from = "\\begin", *until = "\\end", *hline = "\\hline", *hdashline = "\\hdashline";
   char *s = newstr, *e = newstr, *attr_strings = "";
-  int n_count = 0, start = 0, end = 0, offset = 0, attr_strings_len = 0;
+  stackElementT stack_item, last_stack_item;
+  int n_count = 0, start = 0, end = 0, offset = 0, attr_strings_len = 0, str_len = 0;
+  hlineDataArray hline_data_array;
+  hlineData hline_data;
 
-  while (1) {
-    // find the starting match
-    s = strstr(s, from);
-    if (!s) break;
-    start = (int)(newstr - s);
+  // set up the array stack
+  StackInit(&array_stack, strlen(newstr));
+  initHlineDataArray(&hline_data_array, 5);
 
-    // find the ending match
-    e = strstr(e, until);
-    if (!e) break;
-    end = (int)(newstr - e);
+  // if none of the array environments, or hline/hdashline, exist, don't bother going on
+  if ((strstr(string, from) == NULL && strstr(string, until) == NULL) || (strstr(string, hline) == NULL && strstr(string, hdashline) == NULL))
+    return string;
 
-    // how big is this string?
-    int array_length = abs(end - start);
+  while (line != NULL) {
+    str_len = strlen(line) + 1;
 
-    // this is just the "from - until" string chunk
-    chunk = substring(newstr, abs(start), array_length);
+    stack_item.line = line;
+    stack_item.line_pos = start + 1;
 
-    printf("dat chunk tho %s", chunk);
+    start += str_len;
 
-    char *line = strtok(chunk, "\n");
-    while(line != NULL)
-    {
-      if (n_count >= 2) {
+    StackPush(&array_stack, stack_item);
+
+    if (strstr(line, until) != NULL) {
+      while (!StackIsEmpty(&array_stack) && strstr(StackTop(&array_stack).line, from) == NULL) {
+        last_stack_item = StackPop(&array_stack);
         attr_strings_len = strlen(attr_strings);
 
         // looking for a line match
-        if (strstr(line, hline) != NULL) {
+        if (strstr(last_stack_item.line, hline) != NULL) {
           if (attr_strings_len > 0)
             remove_last_char(attr_strings);
           attr_strings = join(attr_strings, "s");
         }
-        else if (strstr(line, hdashline) != NULL) {
+        else if (strstr(last_stack_item.line, hdashline) != NULL) {
           if (attr_strings_len > 0)
             remove_last_char(attr_strings);
           attr_strings = join(attr_strings, "d");
@@ -154,27 +255,53 @@ char * hline_replace(const char *string) {
           attr_strings = join(attr_strings, "0");
         }
       }
-      else
-        n_count++;
 
-      line = strtok(NULL, "\n");
+      if (!StackIsEmpty(&array_stack)) // should never be empty, assuming appropriately closed array
+        last_stack_item = StackPop(&array_stack); // now, modify the starting \begin with the hline info
+
+      if (attr_strings_len != 0) {
+        // array is form of \begin{array}[t]{cc..c}
+        if ( (tok = strstr(last_stack_item.line, "]{")) != NULL && strncmp(attr_strings, "", 1) != 0) {
+          offset = last_stack_item.line_pos + (tok - last_stack_item.line);
+          // we cut the last char because we can skip the first row
+          remove_last_char(attr_strings);
+          // we reverse the string, because we're going backwards
+          strrev(attr_strings);
+          attr_strings = join(join("(", attr_strings), ")");
+          hline_data.attr_strings = strdup(attr_strings);
+          hline_data.offset_pos = offset + 1;
+          insertHlineDataArray(&hline_data_array, hline_data);
+        }
+        // array is form of \begin{array}{cc..c}
+        else if ( (tok = strstr(last_stack_item.line, "}{")) != NULL && strncmp(attr_strings, "", 1) != 0) {
+          offset = last_stack_item.line_pos + (tok - last_stack_item.line);
+          // we cut the last char because we can skip the first row
+          remove_last_char(attr_strings);
+          // we reverse the string, because we're going backwards
+          strrev(attr_strings);
+          attr_strings = join(join("(", attr_strings), ")");
+          hline_data.attr_strings = strdup(attr_strings);
+          hline_data.offset_pos = offset + 1;
+          insertHlineDataArray(&hline_data_array, hline_data);
+        }
+      }
+
+      attr_strings = "";
+      attr_strings_len = 0;
     }
 
-    // array is form of \begin{array}[t]{cc..c}
-    if ( (tok = strstr(chunk, "]{")) != NULL && strncmp(attr_strings, "", 1) != 0) {
-      offset = (int)(tok - chunk) + 1;
-      attr_strings = join(join("(", attr_strings), ")");
-      insert_substring(newstr, attr_strings, -start + offset);
-    }
-    // array is form of \begin{array}{cc..c}
-    else if ( (tok = strstr(chunk, "}{")) != NULL && strncmp(attr_strings, "", 1) != 0) {
-      offset = (int)(tok - chunk) + 1;
-      attr_strings = join(join("(", attr_strings), ")");
-      insert_substring(newstr, attr_strings, -start + offset);
-    }
-
-    s = e + 1;
+    line = strtok(NULL, "\n");
   }
+
+  // sort array by highes value first, so that we can insert to newstr from the
+  // bottom to the top (ensuring line numbers stay accurate)
+  sortHLineDataArray(&hline_data_array);
+  for (int i = 0; i < hline_data_array.used; i++) {
+    insert_substring(&newstr, hline_data_array.array[i].attr_strings, hline_data_array.array[i].offset_pos);
+  }
+
+  StackDestroy(&array_stack);
+  deleteHlineDataArray(&hline_data_array);
 
   return newstr;
 }
--- src/parse_extras.h
+++ src/parse_extras.h
@@ -1,4 +1,5 @@
-/* Added methods for string maniuplation */
+/* Added methods for string manipulation */
+
 #ifndef PARSE_EXTRAS_H
 #define PARSE_EXTRAS_H
 
@@ -7,6 +8,20 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+  typedef struct {
+    char *attr_strings;
+    int  offset_pos;
+  } hlineData;
+
+  // array of structs
+  typedef struct
+  {
+    hlineData *array;
+    size_t used;
+    size_t size;
+  } hlineDataArray;
+
   // Replace a substring (`old`) with some text (`new`) in the string `str`.
   extern char *replace_str(const char *str, const char *old, const char *new);
 
@@ -20,8 +35,23 @@ extern "C" {
   // Remove the final character in a string
   extern void remove_last_char(char* str);
 
-  // Insert a substring (`new_str`) into a string (`str`) at `position`
-  extern void insert_substring(char *str, char *new_str, int position);
+  // Insert a substring (`ins`) into a string (`dest`) at `location`
+  extern void insert_substring(char **dest, char *ins, size_t location);
+
+  // Reverse the substring
+  extern void strrev(char *str);
+
+  // set up array of hlineData
+  extern void initHlineDataArray(hlineDataArray *a, size_t initialSize);
+
+  // insert into hlineData array
+  extern void insertHlineDataArray(hlineDataArray *a, hlineData element);
+
+  // sort the array based on offset_pos
+  extern void sortHLineDataArray(hlineDataArray *a);
+
+  // destroyes the array
+  extern void deleteHlineDataArray(hlineDataArray *a);
 
   // Move the `hline` and `hlinedash` symbols inline with the `\begin{array}` line
   // This is so that the Bison parser can properly act on these
