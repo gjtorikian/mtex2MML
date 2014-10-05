add6453131f5404baee02569f828c9e73fd88d34
Garen Torikian <gjtorikian@gmail.com>
2014-10-05 10:29:30 -0500

    Uncomment test

--- test/cornercases_test.rb
+++ test/cornercases_test.rb
@@ -6,15 +6,15 @@ class Mtex2MMLCornerCasesTest < MiniTest::Test
     @mtex = Mtex2MML::Parser.new
   end
 
-#   def test_broken_up_inline_env
-#     text = '''
-# $g\'_{\tau} = \left(
-# \begin{smallmatrix}1&u\\\\0&1\end{smallmatrix} \right) \left(
-# \begin{smallmatrix}v^{1/2}&0\\\\0&v^{-1/2}\end{smallmatrix} \right)$
-# '''
+  def test_broken_up_inline_env
+    text = '''
+$g\'_{\tau} = \left(
+\begin{smallmatrix}1&u\\\\0&1\end{smallmatrix} \right) \left(
+\begin{smallmatrix}v^{1/2}&0\\\\0&v^{-1/2}\end{smallmatrix} \right)$
+'''
 
-#     assert_equal(@mtex.filter(text), fixture_file("broken_up_inline_env"))
-#   end
+    assert_equal(@mtex.filter(text), fixture_file("broken_up_inline_env"))
+  end
 
   def test_some_crazy_alignment
     text = '''
