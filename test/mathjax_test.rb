c517dee18576ca0b52c454e66b523ac62aadbb7c
Garen Torikian <gjtorikian@gmail.com>
2014-10-09 18:36:20 -0500

    Finish up the arrows tests

--- test/mathjax_test.rb
+++ test/mathjax_test.rb
@@ -10,9 +10,15 @@ class MTex2MMLMathJaxTest < MiniTest::Test
   MATHJAX_TEST_TEX_DIR = File.join(MATHJAX_TEST_TEST_DIR, 'LaTeXToMathML-tex')
   MATHJAX_TEST_OUT_DIR = File.join(MATHJAX_TEST_TEST_DIR, 'LaTeXToMathML-out')
 
-  DIRS_WE_DO = %w(above-below).join("|")
+  DIRS_WE_DO = %w(above-below arrows)
+  DIRS_WE_DO_GLOB = "{#{DIRS_WE_DO.join(',')}}"
+  DIRS_IGNORED = %w(action AMScd)
+
+  done_count = Dir["test/fixtures/MathJax/LaTeXToMathML-tex/#{DIRS_WE_DO_GLOB}/*.tex"].length
+  skipped_count = Dir["test/fixtures/MathJax/LaTeXToMathML-tex/#{DIRS_WE_DO_GLOB}/*.xtex"].length
+
   Dir['test/fixtures/MathJax/LaTeXToMathML-tex/**/*.tex'].each do |tex|
-    next unless tex =~ /#{DIRS_WE_DO}/
+    next unless tex =~ /#{DIRS_WE_DO.join("|")}/
     next if File.directory? tex
 
     define_method "test_#{tex}" do
@@ -22,8 +28,13 @@ class MTex2MMLMathJaxTest < MiniTest::Test
       expected = File.read(outfile)
       actual = @mtex.filter(tex_contents)
 
-      write_to_test_file(actual) if tex =~ /underset-1/
+      write_to_test_file(actual) if tex =~ /Newextarrow/
       assert_equal(actual.strip, expected.strip)
+      done_count += 1
     end
   end
+
+  total = (done_count + skipped_count).to_f
+  coverage = done_count.fdiv(total) * 100
+  puts "\n\n*** You did #{done_count} and skipped #{skipped_count}: #{coverage.round(2)}% coverage ***\n\n"
 end
