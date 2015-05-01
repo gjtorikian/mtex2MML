367e5c9b5f1f31b5b58acf8bbd626f5f690b18e0
Garen Torikian <gjtorikian@gmail.com>
2015-04-30 22:52:11 -0700

    Start using clar

---
+++ tests/basic.c
@@ -0,0 +1,24 @@
+/* adding.c for the "Adding" suite */
+#include "clar.h"
+#include "mtex2MML.h"
+
+static int *answer;
+
+void test_basic__initialize(void)
+{
+    answer = malloc(sizeof(int));
+    cl_assert_(answer != NULL, "No memory left?");
+    *answer = 42;
+}
+
+void test_basic__cleanup(void)
+{
+    free(answer);
+}
+
+void test_basic__make_sure_math_still_works(void)
+{
+    cl_assert_(1 > 3, "Five should probably be greater than three");
+    cl_assert_(-5 < 2, "Negative numbers are small, I think");
+    cl_assert_(*answer == 42, "The universe is doing OK. And the initializer too.");
+}
