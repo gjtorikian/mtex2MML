114a7a753e2880d6426fbccc2f68595c91e3eb13
Garen Torikian <gjtorikian@gmail.com>
2015-05-01 15:55:20 -0700

    Bring over MathJax folder

---
+++ tests/mathjax.c
@@ -0,0 +1,24 @@
+#include "clar.h"
+#include "clar_test.h"
+#include <stdio.h>
+
+static char *fixture_tex;
+static char *fixture_mml;
+static char *result;
+
+void test_mathjax__initialize(void)
+{
+  global_test_counter++;
+}
+
+void test_mathjax__cleanup(void)
+{
+  if (fixture_tex != NULL)
+    free(fixture_tex);
+
+  if (fixture_mml != NULL)
+    free(fixture_mml);
+
+  if (result != NULL)
+    free(result);
+}
