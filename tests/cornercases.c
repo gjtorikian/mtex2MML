02ab24c1a7a428bfef67443ef725762f9c3a17f5
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2020-01-06 16:37:18 +0000

    Show faulty logic

--- tests/cornercases.c
+++ tests/cornercases.c
@@ -44,3 +44,12 @@ void test_cornercases__some_crazy_alignment(void)
 
   cl_assert_equal_s(fixture_mml, result);
 }
+
+void test_cornercases__overflow(void)
+{
+  fixture_tex = read_fixture_tex("cornercases/overflow.txt");
+  fixture_mml = read_fixture_mml("cornercases/overflow.html");
+  result = mtex2MML_parse(fixture_tex, strlen(fixture_tex), MTEX2MML_DELIMITER_DOLLAR);
+
+  cl_assert_equal_s(fixture_mml, result);
+}
